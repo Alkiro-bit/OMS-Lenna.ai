68484ede6e2367ce051214dee708b28ed1f6f278
Alkiro-bit <altafazka15@gmail.com>
2026-04-29 12:36:27 +0700

    loginpage component

--- oms-project/src/App.vue
+++ oms-project/src/App.vue
@@ -1,7 +1,23 @@
-<script setup></script>
+<script setup>
+import LoginPage from './components/main/LoginPage.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>  
+  <LoginPage />
 </template>
 
-<style scoped></style>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Jakarta:wght@400;500;600;700;800&display=swap');
+
+:root {
+  font-family: 'Open Jakarta', sans-serif;
+}
+
+html,
+body,
+#app {
+  min-height: 100%;
+  margin: 0;
+  font-family: 'Open Jakarta', sans-serif;
+}
+</style>

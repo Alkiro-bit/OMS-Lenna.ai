f5ee4d900db62d84d6df57ad5efe6ccbd0c28e8c
Alkiro-bit <altafazka15@gmail.com>
2026-04-29 10:45:14 +0700

    git commit

--- oms-project/src/App.vue
+++ oms-project/src/App.vue
@@ -7,7 +7,7 @@
 =======
   <p> test ini adalah praktik pertama menggunakan github collaborative</p>
   <p>test lagi</p>
->>>>>>> 9c8dd8dadd3544a1945b49d9b8ea4796550a609e
+  
 </template>
 
 <style scoped></style>

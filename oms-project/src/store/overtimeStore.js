6eab7e7692e54df0e267a67c1e9dd52d0b625c66
afganteng <apaajabuatdipake@gmail.com>
2026-05-18 22:43:21 +0700

    update: correct IDs and names in pmList

--- oms-project/src/store/overtimeStore.js
+++ oms-project/src/store/overtimeStore.js
@@ -2,9 +2,9 @@ import { ref } from "vue";
 
 export const pmList = ref([
   { id: 9, name: "Mba Annisa" },
-  { id: 2, name: "A Fuja"},
-  { id: 3, name: "Mas FAchry" },
-  { id: 4, name: "Mas Gilang" }
+  { id: 12, name: "A Fuja"},
+  { id: 11, name: "Mas Fachri" },
+  { id: 13, name: "Mas Gilang" }
 ]);
 
 export const overtimeList = ref([]);

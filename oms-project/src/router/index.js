c53f431a487bc88fc32c34e24c86e58e404ccae8
Alkiro-bit <altafazka15@gmail.com>
2026-05-21 10:37:30 +0700

    itu dah

--- oms-project/src/router/index.js
+++ oms-project/src/router/index.js
@@ -3,12 +3,14 @@ import LoginPage from '../components/main/LoginPage.vue'
 import Dashboard from '../components/main/shared/Dashboard.vue'
 import Form from '../components/main/shared/OvertimeForm.vue'
 import ReviewApprovals from '../components/main/pm/ReviewApprovals.vue'
+import DashboardHr from '../components/main/hr/DashboardHr.vue'
 
 const routes = [
   { path: '/', name: 'Login', component: LoginPage },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
   { path: '/form', name: 'Form', component: Form },
   { path: '/review-approvals', name: 'Review Approvals', component: ReviewApprovals },
+  { path: '/dashboard-hr', name: 'HR Dashboard', component: DashboardHr },
 ]
 
 const router = createRouter({
